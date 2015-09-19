8e7b85d84b7b9925dffb694c86d048c73186855c
Ben Sarmiento <me@bensarmiento.com>
2015-09-19 20:02:07 +0800

    proper rendering

--- app/modules/home/home.controller.js
+++ app/modules/home/home.controller.js
@@ -1,11 +1,19 @@
-define([], function () {
+define(['jsd'], function (Diagram) {
   'use strict';
 
-  controller.$inject = [];
+  controller.$inject = ['$scope'];
 
-  function controller() {
+  function controller(scope) {
     var self = this;
     self.source_code = 'A->B: Hello World!';
+
+    scope.$watch(function() { return self.source_code; }, function(val) {
+      if (!val) return;
+
+      Diagram.parse(val).drawSVG('diagram');
+      var diagram_div = document.querySelectorAll('#diagram')[0];
+      var svg = diagram_div.querySelector('svg');
+    });
   }
 
   return controller;
